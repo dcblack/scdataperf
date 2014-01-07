78591b0dc2e2fb93edca508d0ba46c5fe58fe76b
David C Black <dcblack@hldwizard.com>
2014-01-06 22:47:04 -0600

    Ready for consumption

---
+++ README.md
@@ -0,0 +1,12 @@
+The source code accompanying this readme contains
+example code to measure the run-time performance
+efficiency of various SystemC data-types. If you
+wish to run this code yourself, you will need:
+
+1. C++ compiler with C++11 features
+   * Suggest: clang 5.0 or gcc 4.7 or Visual C++ 2013
+2. SystemC 2.3 or better
+   * Obtain from accellera.org
+
+Examine the log file for an example of how to compile
+and run this example.
--- dataperf.cpp
+++ dataperf.cpp
@@ -1,7 +1,25 @@
 // FILE: dataperf.cpp
+//
 // DESCRPTION:
+//
 //   Compare relative performance of data types in SystemC
 //   by creating long loops and performing arithmetic.
+//
+// LICENSE:
+//
+//   Copyright 2014 David C Black
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 
 #include "require_cxx11.h"
 #include <cstdint>
@@ -113,3 +131,5 @@ int sc_main(int argc, char* argv[])
 
   return 0;
 }
+
+//End of file
